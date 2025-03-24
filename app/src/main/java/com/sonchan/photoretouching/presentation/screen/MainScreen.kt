66a600a344d48c63c2da079e71ff485bd5f23bc7
robo710 <sonchanhyeong710@gmail.com>
2025-03-24 10:37:34 +0900

    feat: mainScreen.kt

---
+++ app/src/main/java/com/sonchan/photoretouching/presentation/screen/MainScreen.kt
@@ -0,0 +1,18 @@
+package com.sonchan.photoretouching.presentation.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MainScreen(
+    modifier: Modifier = Modifier
+){
+
+}
+
+@Preview
+@Composable
+fun MainScreenPreview(){
+    MainScreen()
+}
