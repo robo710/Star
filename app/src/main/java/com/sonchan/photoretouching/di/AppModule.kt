86ec8cafe18bc8bf56b8b11447a87958b3f45e02
손찬형 <sonchanhyeong710@gmail.com>
2025-04-02 14:00:04 +0900

    feat: AppModule 설정

--- app/src/main/java/com/sonchan/photoretouching/di/AppModule.kt
+++ app/src/main/java/com/sonchan/photoretouching/di/AppModule.kt
@@ -1,4 +1,19 @@
 package com.sonchan.photoretouching.di
 
-class AppModule {
+import android.app.Activity
+import com.sonchan.photoretouching.data.repository.main.GalleryRepositoryImpl
+import com.sonchan.photoretouching.domain.repository.main.GalleryRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+
+@Module
+@InstallIn(ActivityComponent::class)
+object AppModule {
+
+    @Provides
+    fun provideGalleryRepository(activity: Activity): GalleryRepository {
+        return GalleryRepositoryImpl(activity)
+    }
 }
