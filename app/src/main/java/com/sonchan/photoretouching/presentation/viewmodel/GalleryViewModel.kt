8cf21e5564815e42b651e133429075965c97a62a
손찬형 <sonchanhyeong710@gmail.com>
2025-04-08 08:55:46 +0900

    feat: uriToBitmap 구현

--- app/src/main/java/com/sonchan/photoretouching/presentation/viewmodel/GalleryViewModel.kt
+++ app/src/main/java/com/sonchan/photoretouching/presentation/viewmodel/GalleryViewModel.kt
@@ -1,6 +1,11 @@
 package com.sonchan.photoretouching.presentation.viewmodel
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sonchan.photoretouching.domain.usecase.main.GetGalleryImageUseCase
@@ -17,6 +22,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class GalleryViewModel @Inject constructor(
+    private val context: Context,
     private val getGalleryImageUseCase: GetGalleryImageUseCase,
     private val setGalleryImageUseCase: SetGalleryImageUseCase
 ): ViewModel() {
@@ -46,6 +52,19 @@ class GalleryViewModel @Inject constructor(
     fun requestOpenGallery(){
         viewModelScope.launch {
             _openGalleryEvent.emit(Unit)
+        }
+    }
+
+    fun uriToBitmap(uri: Uri): Bitmap?{
+        return try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+                val source = ImageDecoder.createSource(context.contentResolver, uri)
+                ImageDecoder.decodeBitmap(source)
+            } else {
+                MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
+            }
+        } catch (e: Exception){
+            null
         }
     }
 }
