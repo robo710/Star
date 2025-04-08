70b362ced27399aae21e416e82f58b660f8fd8f3
손찬형 <sonchanhyeong710@gmail.com>
2025-04-08 09:12:35 +0900

    feat: SaveImageToGalleryUseCase 생성

---
+++ app/src/main/java/com/sonchan/photoretouching/domain/usecase/main/SaveImageToGalleryUseCase.kt
@@ -0,0 +1,14 @@
+package com.sonchan.photoretouching.domain.usecase.main
+
+import android.graphics.Bitmap
+import com.sonchan.photoretouching.domain.model.ImageFormat
+import com.sonchan.photoretouching.domain.repository.main.GalleryRepository
+import javax.inject.Inject
+
+class SaveImageToGalleryUseCase @Inject constructor(
+    private val repository: GalleryRepository
+) {
+    suspend operator fun invoke(bitmap: Bitmap, format: ImageFormat): Boolean{
+        return repository.saveImage(bitmap, format)
+    }
+}
