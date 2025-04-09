bc83d01892607df9fe175848bab5d392ca53525e
손찬형 <sonchanhyeong710@gmail.com>
2025-04-09 09:09:38 +0900

    feat: RetouchingToast파일 생성

---
+++ app/src/main/java/com/sonchan/photoretouching/presentation/component/RetouchingToast.kt
@@ -0,0 +1,15 @@
+package com.sonchan.photoretouching.presentation.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RetouchingToast(){
+
+}
+
+@Preview
+@Composable
+fun RetouchingToastPreview(){
+    RetouchingToast()
+}
