0237cd5098d26b9b89f3b6acd0b7e8bae7ec46e9
손찬형 <sonchanhyeong710@gmail.com>
2025-04-10 18:24:17 +0900

    refactor: remove unused import

--- app/src/main/java/com/sonchan/photoretouching/presentation/component/RetouchingSlider.kt
+++ app/src/main/java/com/sonchan/photoretouching/presentation/component/RetouchingSlider.kt
@@ -5,28 +5,19 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Slider
-import androidx.compose.material3.SliderDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.sonchan.photoretouching.R
 import com.sonchan.photoretouching.ui.theme.PhotoRetouchingTheme
 
 @Composable
