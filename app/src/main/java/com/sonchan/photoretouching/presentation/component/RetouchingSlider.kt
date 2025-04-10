5c1de4378c7e8bf77b727a112e84823062ee9cd4
손찬형 <sonchanhyeong710@gmail.com>
2025-04-11 08:59:26 +0900

    refactor: remove unused import

--- app/src/main/java/com/sonchan/photoretouching/presentation/component/RetouchingSlider.kt
+++ app/src/main/java/com/sonchan/photoretouching/presentation/component/RetouchingSlider.kt
@@ -1,7 +1,6 @@
 package com.sonchan.photoretouching.presentation.component
 
 import android.content.res.Configuration
-import android.text.Layout
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
--- app/src/main/java/com/sonchan/photoretouching/presentation/screen/RetouchingScreen.kt
+++ app/src/main/java/com/sonchan/photoretouching/presentation/screen/RetouchingScreen.kt
@@ -1,7 +1,6 @@
 package com.sonchan.photoretouching.presentation.screen
 
 import android.net.Uri
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
