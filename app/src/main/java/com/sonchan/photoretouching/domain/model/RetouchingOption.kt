725d98581a53e52a5184fdb593ea937623d3a5ab
손찬형 <sonchanhyeong710@gmail.com>
2025-04-10 10:54:08 +0900

    feat: RetouchingOption enum 제작

--- app/src/main/java/com/sonchan/photoretouching/domain/model/RetouchingOption.kt
+++ app/src/main/java/com/sonchan/photoretouching/domain/model/RetouchingOption.kt
@@ -3,16 +3,21 @@ package com.sonchan.photoretouching.domain.model
 import androidx.annotation.DrawableRes
 import com.sonchan.photoretouching.R
 
-enum class RetouchingOption(val label: String, @DrawableRes val icon: Int) {
-    LIGHT_BALANCE("라이트 밸런스", R.),
-    BRIGHTNESS("밝기", R.drawable.ic_brightness),
-    EXPOSURE("노출", R.drawable.ic_exposure),
-    CONTRAST("대비", R.drawable.ic_contrast),
-    HIGHLIGHT("하이라이트", R.drawable.ic_highlight),
-    SHADOW("그림자", R.drawable.ic_shadow),
-    SATURATION("채도", R.drawable.ic_saturation),
-    TINT("틴트", R.drawable.ic_tint),
-    TEMPERATURE("색온도", R.drawable.ic_temperature),
-    SHARPNESS("선명도", R.drawable.ic_sharpness),
-    CLARITY("명료도", R.drawable.ic_clarity)
+enum class RetouchingOption(
+    val label: String,
+    @DrawableRes val icon: Int,
+    val range: ClosedFloatingPointRange<Float> = -100f..100f,
+    val defaultValue: Float = 0f
+) {
+    LIGHT_BALANCE("라이트 밸런스", R.drawable.light_balance_icon),
+    BRIGHTNESS("밝기", R.drawable.brightness_icon),
+    EXPOSURE("노출", R.drawable.exposure_icon),
+    CONTRAST("대비", R.drawable.contrast_icon),
+    HIGHLIGHT("하이라이트", R.drawable.highlight_icon),
+    SHADOW("그림자", R.drawable.shadow_icon),
+    SATURATION("채도", R.drawable.saturation_icon),
+    TINT("틴트", R.drawable.tint_icon),
+    TEMPERATURE("색온도", R.drawable.temperature_icon),
+    SHARPNESS("선명도", R.drawable.sharpness_icon),
+    CLARITY("명료도", R.drawable.clarity_icon)
 }
