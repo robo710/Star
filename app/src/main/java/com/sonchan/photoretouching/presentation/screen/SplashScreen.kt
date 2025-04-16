c37ea39696cd9aef1a17d6e32a70a79f260201f5
손찬형 <sonchanhyeong710@gmail.com>
2025-04-16 14:59:08 +0900

    feat: splashScreen 구성

--- app/src/main/java/com/sonchan/photoretouching/presentation/screen/SplashScreen.kt
+++ app/src/main/java/com/sonchan/photoretouching/presentation/screen/SplashScreen.kt
@@ -1,4 +1,65 @@
 package com.sonchan.photoretouching.presentation.screen
 
-class SplashScreen {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.sonchan.photoretouching.R
+import com.sonchan.photoretouching.presentation.component.DarkThemeDevicePreview
+import com.sonchan.photoretouching.presentation.component.DevicePreviews
+import com.sonchan.photoretouching.ui.theme.PhotoRetouchingTheme
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(
+    modifier: Modifier = Modifier,
+    onNavigateToMain: () -> Unit
+) {
+    LaunchedEffect(Unit){
+        delay(1000L)
+        onNavigateToMain
+    }
+
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        contentAlignment = Alignment.Center
+    ){
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(id = R.drawable.splash_icon),
+                contentDescription = "Logo",
+                modifier = Modifier.size(150.dp)
+            )
+            Spacer(modifier = Modifier.height(20.dp))
+            Text(
+                text = "Star",
+                color = MaterialTheme.colorScheme.onBackground
+            )
+        }
+    }
+}
+
+@DevicePreviews
+@DarkThemeDevicePreview
+@Composable
+fun SplashScreenPreview(){
+    PhotoRetouchingTheme {
+        SplashScreen(
+            onNavigateToMain = {}
+        )
+    }
 }
