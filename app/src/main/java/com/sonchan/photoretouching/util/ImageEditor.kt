96ef20085bfba6d84d0fd6b7be5fd5c57f5fe261
손찬형 <sonchanhyeong710@gmail.com>
2025-04-16 09:42:29 +0900

    feat: ImageEditor 생성

--- app/src/main/java/com/sonchan/photoretouching/util/ImageEditor.kt
+++ app/src/main/java/com/sonchan/photoretouching/util/ImageEditor.kt
@@ -1,4 +1,29 @@
 package com.sonchan.photoretouching.util
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
+import android.graphics.Paint
+
 object ImageEditor {
+    fun applyBrightness(src: Bitmap, value: Int): Bitmap {
+        val bmp = Bitmap.createBitmap(src.width, src.height, src.config ?: Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bmp)
+        val paint = Paint()
+        val brightness = value / 100f * 255
+
+        val colorMatrix = ColorMatrix(
+            floatArrayOf(
+                1f, 0f, 0f, 0f, brightness,
+                0f, 1f, 0f, 0f, brightness,
+                0f, 0f, 1f, 0f, brightness,
+                0f, 0f, 0f, 1f, 0f
+            )
+        )
+
+        paint.colorFilter = ColorMatrixColorFilter(colorMatrix)
+        canvas.drawBitmap(src, 0f, 0f, paint)
+        return bmp
+    }
 }
