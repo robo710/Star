79d028d4fc0bb7c9822b20df80c4588f51c6f9db
손찬형 <sonchanhyeong710@gmail.com>
2025-04-17 20:56:14 +0900

    refactor: HighlighterFilter, ShadowFilter 복구

--- app/src/main/java/com/sonchan/photoretouching/util/ImageEditor.kt
+++ app/src/main/java/com/sonchan/photoretouching/util/ImageEditor.kt
@@ -2,11 +2,12 @@ package com.sonchan.photoretouching.util
 
 import android.content.Context
 import android.graphics.Bitmap
+import com.sonchan.photoretouching.gpu.HighlightFilter
+import com.sonchan.photoretouching.gpu.ShadowFilter
 import jp.co.cyberagent.android.gpuimage.GPUImage
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageBrightnessFilter
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageContrastFilter
 import jp.co.cyberagent.android.gpuimage.filter.GPUImageExposureFilter
-import jp.co.cyberagent.android.gpuimage.filter.GPUImageHighlightShadowFilter
 
 object ImageEditor {
     fun applyBrightness(context: Context, bitmap: Bitmap, value: Int): Bitmap {
@@ -34,19 +35,19 @@ object ImageEditor {
     }
 
     fun applyHighlight(context: Context, bitmap: Bitmap, value: Int): Bitmap {
-        val intensity = value / 100f
+        val intensity = value / 150f
         val gpuImage = GPUImage(context)
         gpuImage.setImage(bitmap)
-        gpuImage.setFilter(GPUImageHighlightShadowFilter(intensity, 0f))
+        gpuImage.setFilter(HighlightFilter(intensity))
         return gpuImage.bitmapWithFilterApplied
     }
 
 
     fun applyShadow(context: Context, bitmap: Bitmap, value: Int): Bitmap {
-        val intensity = value / 100f
+        val intensity = value / 300f
         val gpuImage = GPUImage(context)
         gpuImage.setImage(bitmap)
-        gpuImage.setFilter(GPUImageHighlightShadowFilter(0f, intensity))
+        gpuImage.setFilter(ShadowFilter(intensity))
         return gpuImage.bitmapWithFilterApplied
     }
 }
