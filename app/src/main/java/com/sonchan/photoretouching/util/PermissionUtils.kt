741867e64008677a182baca8d038dd847bdfd8ef
손찬형 <sonchanhyeong710@gmail.com>
2025-04-28 19:48:27 +0900

    feat: PermissionUtils 구현

--- app/src/main/java/com/sonchan/photoretouching/util/PermissionUtils.kt
+++ app/src/main/java/com/sonchan/photoretouching/util/PermissionUtils.kt
@@ -1,4 +1,17 @@
 package com.sonchan.photoretouching.util
 
+import android.Manifest
+import android.os.Build
+
 object PermissionUtils {
+    val galleryPermissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        arrayOf(
+            Manifest.permission.READ_MEDIA_IMAGES
+        )
+    } else {
+        arrayOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+    }
 }
